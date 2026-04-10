e593b95f8bb4e2048b10ae8f79916a418c5750d5
salithbinanwar <salithanwar69@gmail.com>
2026-04-10 15:36:26 +0800

    api added

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true, // Important for cookies
+  baseURL: 'https://grabitserver.onrender.com/api',
+  withCredentials: true,
 })
 
 export default API
